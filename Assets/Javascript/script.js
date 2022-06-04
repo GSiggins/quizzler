1dd0d63d79dd8d4fecb249c9cdf050e0dde47d36
GSiggins <gregory.siggins@gmail.com>
2022-06-03 22:13:59 -0700

    set placeholders for hide/display screens for correct/incorrect, nested objects to hold properties of true/false

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -8,6 +8,12 @@ var startBtn = document.getElementById("startBtn")
 var questions = document.querySelector(".questions")
 var intro = document.querySelector(".intro")
 var question = document.getElementById("question")
+var buttons = document.getElementById('buttons')
+var gameOver = document.getElementById('game-over')
+var correct = document.getElementById('correct')
+var incorrect = document.getElementById('incorrect')
+var questionIndex = 0
+var questionsArray = [question1, question2, question3, question4, question5, question6, question7]
 
 // init()
 function startGame() {
@@ -28,99 +34,109 @@ function setTime() {
     }
   }, 1000);
 }
+
 // event listener for start game
 function chooseQ() {
-  // for (let i = 0; i < questionsArray.length; i++) {
-    const chosenQIndex = Math.floor(Math.random() * questionsArray.length)
-    console.log(chosenQIndex)
-  // }
+  const chosenQIndex = Math.floor(Math.random() * questionsArray.length)
+  console.log(chosenQIndex)
   return questionsArray[chosenQIndex]
 }
 
-function displayQ(chosenQ) {
-    // const qArray = Object.values(chosenQ);
-    question.textContent = chosenQ.Question;
-    answerA.textContent = chosenQ.A;
-    answerB.textContent = chosenQ.B;
-    answerC.textContent = chosenQ.C;
-    answerD.textContent = chosenQ.D;
-  }
-
-
-
-  // for (let j = 0; j < qArray.length; j++) {
-  //   document.getElementById("question").innerHTML = [j].Question;
-  //   document.getElementById("A").innerHTML = [j].A;
-  //   document.getElementById("B").innerHTML = [j].B;
-  //   document.getElementById("C").innerHTML = [j].C;
-  //   document.getElementById("D").innerHTML = [j].D;
-  // }
+function displayQ(chosenQ, chosenQIndex) {
+  // const qArray = Object.values(chosenQ);
+  question.textContent = chosenQ.Question;
+  answerA.textContent = chosenQ.A.answerText;
+  answerB.textContent = chosenQ.B.answerText;
+  answerC.textContent = chosenQ.C.answerText;
+  answerD.textContent = chosenQ.D.answerText;
+  questionsArray.splice(chosenQIndex, 1);
+  console.log(questionsArray);
+}
 
 //when questions answered or timer === 0 game over
+function timeUp () {
+  gameOver.setAttribute("style", "display:block");
+  questions.setAttribute("style", "display:none");
+  setTimeout(gameOver.setAttribute("style", "display:none"), questions.setAttribute("style", "display:block"), 5000);
+  return;
+}
+
+function checkAnswer(event)
 
 // save initials and score
+function correct() {
+  correct.setAttribute("style", "display:block");
+  buttons.setAttribute("style", "display:none");
+  setTimeout(correct.setAttribute("style", "display:none"), buttons.setAttribute("style", "display:block"), 2000);
+  return;
+}
+
+function incorrect(secondsLeft) {
+  incorrect.setAttribute("style", "display:block");
+  buttons.setAttribute("style", "display:none");
+  setTimeout(incorrect.setAttribute("style", "display:none"), buttons.setAttribute("style", "display:block"), 2000);
+  secondsLeft - 3;
+  return;
+}
+
+
 
 const question1 = {
   Question: "What kind of primitive type refers to a true or false value?",
-  A: "Boolean",
-  B: "Variable",
-  C: "String",
-  D: "Number"
+  A: { answerText: "Boolean", correct: 'true' },
+  B: { answerText: "Variable", correct: 'false' },
+  C: { answerText: "String", correct: 'false' },
+  D: { answerText: "Number", correct: 'false' }
 };
 
 const question2 = {
   Question: "What holds a collection of items, referred to by indices?",
-  A: "String",
-  B: "Boolean",
-  C: "Array",
-  D: "List"
+  A: { answerText: "String", correct: 'false' },
+  B: { answerText: "Boolean", correct: 'false' },
+  C: { answerText: "Array", correct: 'true' },
+  D: { answerText: "List", correct: 'false' }
 }
 
 const question3 = {
   Question: "What is the term for using JavaScript to access specific parts of the HTML?",
-  A: "Third party API",
-  B: "Traversing the DOM",
-  C: "Pathing via javascript",
-  D: "Navigating the HTML"
+  A: { answerText: "Third party API", correct: 'false' },
+  B: { answerText: "Traversing the DOM", correct: 'true' },
+  C: { answerText: "Pathing via javascript", correct: 'false' },
+  D: { answerText: "Navigating the HTML", correct: 'false' }
 }
 
 const question4 = {
   Question: "What is a section of code that runs repeatedly until conditions are met?",
-  A: "Loop",
-  B: "Object",
-  C: "Element",
-  D: "Variable"
+  A: { answerText: "Loop", correct: 'true' },
+  B: { answerText: "Object", correct: 'false' },
+  C: { answerText: "Element", correct: 'false' },
+  D: { answerText: "Variable", correct: 'false' }
 }
 
 const question5 = {
   Question: "What is used to create an arbitrary name given to a specific piece of information?",
-  A: "Variable",
-  B: "String",
-  C: "Boolean",
-  D: "Array"
+  A: { answerText: "Variable", correct: 'true' },
+  B: { answerText: "String", correct: 'false' },
+  C: { answerText: "Boolean", correct: 'false' },
+  D: { answerText: "Array", correct: 'false' }
 }
 
 const question6 = {
   Question: "What allows elements within HTML to arrange themselves according to element size and space in a parent element?",
-  A: "Flow-square",
-  B: "Justify-content",
-  C: "Media-query",
-  D: "Flex-box"
+  A: { answerText: "Flow-square", correct: 'false' },
+  B: { answerText: "Justify-content", correct: 'false' },
+  C: { answerText: "Media-query", correct: 'false' },
+  D: { answerText: "Flex-box", correct: 'true' }
 }
 
 const question7 = {
   Question: "What is it called when an app rescales according to screen size?",
-  A: "Scaling Design",
-  B: "Media Rescaling",
-  C: "Responsive Design",
-  D: "Screen resizing"
+  A: { answerText: "Scaling Design", correct: 'false' },
+  B: { answerText: "Media Rescaling", correct: 'false' },
+  C: { answerText: "Responsive Design", correct: 'true' },
+  D: { answerText: "Screen resizing", correct: 'false' }
 }
 
 startBtn.addEventListener("click", startGame)
-
-var questionIndex = 0
-var questionsArray = [question1, question2, question3, question4, question5, question6, question7]
-
-
-
+buttons.addEventListener("click", checkAnswer)
 startBtn.addEventListener("click", startGame)
